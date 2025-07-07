7f26c53faf1d6331bf593d8d1e2aee49b0b4f180
vagmin27 <vagmin2727@gmail.com>
2025-07-07 14:44:32 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
+import { app } from './app.js';
 import connectDB from "./db/index.js";
 
 dotenv.config({
