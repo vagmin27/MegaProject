b4ee6356270e4f6bd6d5b13b43180f894dce07f9
vagmin27 <vagmin2727@gmail.com>
2025-07-11 18:21:14 +0530

    .

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,20 @@
+// db/index.js
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
-    }
-}
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+        dbName: DB_NAME,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        });
 
-export default connectDB
+        console.log(`✅ MongoDB connected! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("❌ MongoDB connection FAILED:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
